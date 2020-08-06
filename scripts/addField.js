17b21fa03314f7e4bcf9313d78ec69cc8b965c2a
Lucas <lxavier.00@outlook.com>
2020-08-06 13:54:45 -0300

    Add funcionalidade de adiçao de horario

--- scripts/addField.js
+++ scripts/addField.js
@@ -1,1 +1,29 @@
-document.querySelector('#container').style.backgroundColor = "Red"
+//Funcionalidade | Adicionar um novo horário
+
+//Procurar o botão 
+document.querySelector("#add-time")
+    //Quando clicar no botão
+    .addEventListener('click', cloneField)
+
+//Executar uma função
+function cloneField() {
+    //Duplicar os campos. Que campo?
+    const newFieldContainer = document.querySelector('.schedule-item').cloneNode(true) //boolean
+
+    //pegando os campos: 
+    const fields = newFieldContainer.querySelectorAll('input')
+
+
+    //para cada campo, limpar
+    fields.forEach(function(field) {
+        //pega o field do momento e limpa
+        field.value = ""
+
+    })
+
+
+    // Colocar na pagina : onde?
+    document.querySelector('#schedule-items').appendChild(newFieldContainer)
+
+
+}
