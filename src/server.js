7a81c048b21c1c882b3c66b67694f5e5b69575a9
Lucas <lxavier.00@outlook.com>
2020-08-07 16:05:29 -0300

    Alguns ajustes

--- src/server.js
+++ src/server.js
@@ -21,17 +21,31 @@ const proffys = [{
         weekday: [1], //Dia da semana
         time_from: [720],
         time_to: [1228]
+    },
+    {
+        name: "Lucas Xavier",
+        avatar: "https://avatars2.githubusercontent.com/u/52923100?s=460&u=96872a605f8062a10d07c8cc78351f3bcb8021ee&v=4",
+        whatsapp: "11201410",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "40",
+        weekday: [1], //Dia da semana
+        time_from: [720],
+        time_to: [1228]
     }
 ]
 
 function pageLanding(req, res) {
+    // console.log(__dirname);
+
+    // /home/lucas/Documentos/Projetos Web/nlw/src
     // return res.sendFile(__dirname + "/view/index.html") SEM O NUNJUCKS
     return res.render("index.html")
 }
 
 function pageStudy(req, res) {
     // return res.sendFile(__dirname + "/view/study.html") SEM O NUNJUCKS
-    return res.render("study.html")
+    return res.render("study.html", { proffys })
 }
 
 function pageGiveClasses(req, res) {
@@ -58,7 +72,3 @@ server
     .get("/give-classes", pageGiveClasses)
 
 .listen(5500)
-
-// console.log(__dirname);
-
-// /home/lucas/Documentos/Projetos Web/nlw/src
