d2899e7832f53ac1d527e9dbd8a80a99c4d21697
Lucas <lxavier.00@outlook.com>
2020-08-09 15:09:35 -0300

    Arrumar a parte de visualizaçao dos proffys

--- src/pages.js
+++ src/pages.js
@@ -53,23 +53,46 @@ async function pageStudy(req, res) {
 }
 
 function pageGiveClasses(req, res) {
-    // return res.sendFile(__dirname + "/view/give-classes.html") SEM O NUNJUCKS
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0 //transformando em um array
-        //Se tiver dados (data)
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-
-        //Add data ao inicio da lista
-        proffys.push(data)
-
-        //AJUDAR NO DESAFIO !!!!
-        return res.redirect('/study')
-    }
-    //Add dados a lista de Proffys
-    //Se não , não adicionar
     return res.render("give-classes.html", { subjects, weekdays })
 }
 
-module.exports = { pageLanding, pageStudy, pageGiveClasses }
+async function saveClasses(req, res) {
+    const createProffy = require('./database/createProffy')
+
+    const proffyValue = {
+        name: req.body.name,
+        avatar: req.body.avatar,
+        whatsapp: req.body.whatsapp,
+        bio: req.body.bio
+    }
+
+    const classValue = {
+        subject: req.body.subject,
+        cost: req.body.cost
+    }
+
+    const classScheduleValues = req.body.weekday.map((weekday, index) => {
+        return {
+            weekday,
+            time_from: convertHoursToMinutes(req.body.time_from[index]),
+            time_to: convertHoursToMinutes(req.body.time_to[index])
+        }
+    })
+
+    try {
+        const db = await Database
+        await createProffy(db, { proffyValue, classValue, classScheduleValues })
+
+        let queryString = "?subject=" + req.body.subject
+        queryString += "&weekday=" + req.body.weekday[0]
+        queryString += "&time=" + req.body.time_from[0]
+
+
+        return res.redirect("/study" + queryString)
+    } catch (error) {
+        console.log(error);
+    }
+
+}
+
+module.exports = { pageLanding, pageStudy, pageGiveClasses, saveClasses }
--- src/server.js
+++ src/server.js
@@ -2,7 +2,12 @@
 const express = require('express')
 const server = express()
 
-const { pageLanding, pageStudy, pageGiveClasses } = require('./pages.js')
+const {
+    pageLanding,
+    pageStudy,
+    pageGiveClasses,
+    saveClasses
+} = require('./pages')
 
 //Configurar nunjucks (Template Engine)
 const nunjucks = require('nunjucks')
@@ -11,15 +16,17 @@ nunjucks.configure('src/view', {
     noCache: true,
 })
 
-
 //Inicio e Configuração do Servidor
 server
-//Configurar arquivos estáticos (css, scripts, images)
+//Receber os dados do req.body
+    .use(express.urlencoded({ extended: true }))
+    //Configurar arquivos estáticos (css, scripts, images)
     .use(express.static("public"))
     //rotas da aplicação
     .get("/", pageLanding)
     .get("/study", pageStudy)
     .get("/give-classes", pageGiveClasses)
+    .post("/save-classes", saveClasses)
 
 //Start do Servidor
 .listen(5500)
