63e9a9a1d04fd5fa4d3528bb3c7f4ff5d2250d63
Lucas <lxavier.00@outlook.com>
2020-08-08 22:20:16 -0300

    Refatoraçao Parte 01

---
+++ src/database/fake_data.js
@@ -0,0 +1,36 @@
+//Dados da Pagina Study
+const proffys = [{
+        name: "Diego Fernandes",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "11201410",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "40",
+        weekday: [0],
+        time_from: [720],
+        time_to: [1228]
+    },
+
+    {
+        name: "Daniele Evangelista",
+        avatar: "https://avatars2.githubusercontent.com/u/2254731?s=460&amp;u=0ba16a79456c2f250e7579cb388fa18c5c2d7d65&amp;v=4",
+        whatsapp: "11201410",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "40",
+        weekday: [1], //Dia da semana
+        time_from: [720],
+        time_to: [1228]
+    },
+    {
+        name: "Lucas Xavier",
+        avatar: "https://avatars2.githubusercontent.com/u/52923100?s=460&u=96872a605f8062a10d07c8cc78351f3bcb8021ee&v=4",
+        whatsapp: "11201410",
+        bio: "Entusiasta das melhores tecnologias de química avançada. Apaixonado por explodir coisas em laboratório e por mudar a vida das pessoas através de experiências. Mais de 200.000 pessoas já passaram por uma das minhas explosões.",
+        subject: "Química",
+        cost: "40",
+        weekday: [1], //Dia da semana
+        time_from: [720],
+        time_to: [1228]
+    }
+]
---
+++ src/utils/format.js
@@ -0,0 +1,34 @@
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química",
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado",
+]
+
+//Funcionalidades
+function getSubject(subjectNumber) {
+    const arrayPosition = +subjectNumber - 1
+    return subjects[arrayPosition]
+}
+
+module.exports = {
+    subjects,
+    weekdays,
+    getSubject
+}
