defe5b46c0c48c41b2347e7e75d45c9d19320a8f
Lucas <lxavier.00@outlook.com>
2020-08-09 08:09:14 -0300

    Refatoraçao Parte 02

--- src/server.js
+++ src/server.js
@@ -1,41 +1,7 @@
-function pageLanding(req, res) {
-    // console.log(__dirname);
-
-    // /home/lucas/Documentos/Projetos Web/nlw/src
-    // return res.sendFile(__dirname + "/view/index.html") SEM O NUNJUCKS
-    return res.render("index.html")
-}
-
-function pageStudy(req, res) {
-    // return res.sendFile(__dirname + "/view/study.html") SEM O NUNJUCKS
-    const filters = req.query
-    return res.render("study.html", { proffys, filters, subjects, weekdays })
-}
-
-function pageGiveClasses(req, res) {
-    // return res.sendFile(__dirname + "/view/give-classes.html") SEM O NUNJUCKS
-    const data = req.query
-
-    const isNotEmpty = Object.keys(data).length > 0 //transformando em um array
-        //Se tiver dados (data)
-    if (isNotEmpty) {
-        data.subject = getSubject(data.subject)
-
-        //Add data ao inicio da lista
-        proffys.push(data)
-
-        //AJUDAR NO DESAFIO !!!!
-        return res.redirect('/study')
-    }
-    //Add dados a lista de Proffys
-    //Se não , não adicionar
-    return res.render("give-classes.html", { subjects, weekdays })
-}
-
-
 //Servidor
 const express = require('express')
 const server = express()
+const { pageLanding, pageStudy, pageGiveClasses } = require('./pages.js')
 
 //Configurar nunjucks (Template Engine)
 const nunjucks = require('nunjucks')
